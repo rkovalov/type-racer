c75ed69297afce51b673d6e2425578e940495c08
Rudolf Kovalyov <rudolf.kovalyov@gmail.com>
2019-10-03 18:02:44 +0300

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+// import cx from 'classnames';
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import { Button } from 'semantic-ui-react';
+import styles from './App.module.scss';
 
 import { fetchRandomText } from './dataProvider';
-import logo from './logo.svg';
+import logo from './logo.png';
 
 const App: React.FC = () => {
   const [texts, setTexts] = useState<string[]>([]);
@@ -11,24 +13,21 @@ const App: React.FC = () => {
   useEffect(() => {
     fetchRandomText().then(setTexts);
   }, []);
-
+  // tslint:disable-next-line:no-console
+  console.log(styles);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <span />
-        <span />
-        <p>
-          Edit <code>src/App.tsx</code> and save
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <div className={styles.Grow}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
+        </div>
+
+        <h2>TypeRacer Game</h2>
+        <div className="">Increase your typing speed!</div>
+        <br />
+        <Button color="teal" size="large">
+          Start &nbsp; &#x1F680;
+        </Button>
       </header>
     </div>
   );
