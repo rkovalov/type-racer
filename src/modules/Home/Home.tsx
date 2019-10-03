ce7ecaebfee0c2b7bc1385817cfeb1bd6f507d85
Rudolf Kovalyov <rudolf.kovalyov@gmail.com>
2019-10-04 02:21:02 +0300

    added SignInModal

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Button } from 'semantic-ui-react';
 
 import styles from './Home.module.scss';
 import logo from './logo.png';
+import SignInModal from './SignInModal';
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = useCallback(() => setIsModalOpen(true), []);
+  const closeModal = useCallback(() => setIsModalOpen(false), []);
   return (
     <>
       <header className={styles.header}>
@@ -16,9 +20,10 @@ export default function Home() {
         <h2>Type Racer Game</h2>
         <div className="">Increase your typing speed!</div>
         <br />
-        <Button color="teal" size="big">
+        <Button color="teal" size="big" onClick={openModal}>
           Start &nbsp; &#x1F680;
         </Button>
+        <SignInModal open={isModalOpen} onClose={closeModal} />
       </main>
     </>
   );
---
+++ src/modules/Home/SignInModal/SignInModal.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback, useState } from 'react';
+import {
+  Button,
+  Form,
+  Header,
+  Icon,
+  Image,
+  Input,
+  Modal,
+  Radio,
+} from 'semantic-ui-react';
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+type Gender = 'male' | 'female';
+
+const SignInModal = React.memo(({ open, onClose }: Props) => {
+  const [gender, setGender] = useState<Gender>('male');
+  const [nickName, setNickName] = useState('');
+  const changeNickName = useCallback((_, { value }) => setNickName(value), []);
+  const changeGender = useCallback((_, { value }) => setGender(value), []);
+
+  return (
+    <Modal dimmer size="small" open={open} onClose={onClose}>
+      <Modal.Content image>
+        <Image
+          circular
+          size="medium"
+          src={process.env.PUBLIC_URL + `/${gender}.png`}
+        />
+        <Modal.Description>
+          <Header>Profile</Header>
+          <Form>
+            <Form.Field>
+              <Input
+                focus
+                placeholder="Your Nickname..."
+                icon={<Icon name="user" color="blue" />}
+                value={nickName}
+                onChange={changeNickName}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                label="male"
+                name="gender"
+                value="male"
+                checked={gender === 'male'}
+                onChange={changeGender}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                label="female"
+                name="gender"
+                value="female"
+                checked={gender === 'female'}
+                onChange={changeGender}
+              />
+            </Form.Field>
+            <Button disabled={!nickName} type="submit" color="teal">
+              Apply
+            </Button>
+          </Form>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  );
+});
+
+export default SignInModal;
